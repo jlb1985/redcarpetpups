725b8cd20bc2be803c2e6bae635809e25a385a9e
jlb1985 <92070030+jlb1985@users.noreply.github.com>
2022-05-24 19:43:55 -0600

    collect form data

---
+++ src/script.js
@@ -0,0 +1,10 @@
+//Collecting form data
+let name = document.getElementById('name').value;
+console.log(name);
+
+let breed = document.getElementById('breed').value;
+console.log(breed);
+
+//Collect data from dropdownlist
+let category = document.querySelector('#category').value
+console.log(category);
