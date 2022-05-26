fb5d3e10f4560f4918151cc825d92b4927dd20ec
jlb1985 <92070030+jlb1985@users.noreply.github.com>
2022-05-26 00:12:46 -0600

    get method completed linked to db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'redcarpetpups';
 
   constructor(private http: HttpClient){
 
+  }
+
+  ngOnInit(){
+    this.fetchMessages();
+  }
+
+  onMessagesFetch(){
+    this.fetchMessages();
   }
 
   onMessageForm(messages: {yName: string, sSubject: string, mMessage: string}){
@@ -22,5 +30,22 @@ export class AppComponent {
     .subscribe((res) => {
       console.log(res);
     });
+  }
+
+  private fetchMessages(){
+    this.http.get('https://redcarpetpups-eacb9-default-rtdb.firebaseio.com/messages.json')
+    .pipe(map((res) => {
+      const messages = [];
+      for(const key in res){
+        if(res.hasOwnProperty(key)){
+          messages.push({...res[key], id: key})
+        }
+        
+      }
+      return messages;
+    }))
+    .subscribe((res) => {
+      console.log(res);
+    })
   }
 }
